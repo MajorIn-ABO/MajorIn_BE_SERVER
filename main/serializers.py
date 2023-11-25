dfb26c3ad7926ac4312857101d366b3471d45aaa
chojayoung <26006@naver.com>
2023-11-25 22:44:05 +0900

    Add : 중고거래 serializer 추가

--- main/serializers.py
+++ main/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Board, Board_Comment, Board_Like, Board_bookmark, Study, Study_Comment, Study_Like
+from .models import User, Board, Board_Comment, Board_Like, Board_bookmark, Study, Study_Comment, Study_Like, Usedbooktrade, Usedbooktrade_Comment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,4 +39,14 @@ class StudyCommentSerializer(serializers.ModelSerializer):
 class StudyLikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Study_Like
-        fields = ['id', 'user_id', 'studypost_id']     
+        fields = ['id', 'user_id', 'studypost_id'] 
+
+class UsedbooktradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usedbooktrade
+        fields = ['id', 'title', 'author', 'seller', 'publisher', 'price', 'imgfile', 'description', 'is_written', 'is_damaged']
+
+class UsedbooktradeCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usedbooktrade_Comment
+        fields = ['id', 'user_id', 'Usedbookpost_id', 'contents']    
