bc4adc5aaad0ca4b626f87c01d5a89621cc6a6fa
chojayoung <26006@naver.com>
2024-03-31 11:29:42 +0900

    Add : requirements.txt

--- majorinBE/settings.py
+++ majorinBE/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import my_settings
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'main',
     'drf_yasg',
     'rest_framework',
+    'common.apps.CommonConfig',
 ]
 
 
